166c71f975f2441bfbb8000dec65be406c04be39
Tae-Soo Pyo <pyo@naoj.org>
2019-10-11 12:29:56 -1000

    1st complete code

--- target_duplication.py
+++ target_duplication.py
@@ -5,6 +5,7 @@
 import sys
 import argparse
 from os.path import basename
+from SubaruTarget import read_SubaruTargetList
 
 parser = argparse.ArgumentParser(description="Check the duplication of targets between the two target files",
                                  usage="{0} <in targetlist txt file> <ref targetlist txt file>".format(basename(__file__)))
@@ -20,4 +21,27 @@ args = parser.parse_args()
 in_file = sys.argv[1]
 ref_file = sys.argv[2]
 
-# check matching of 
+
+
+in_targetlist = read_SubaruTargetList(in_file)
+ref_targetlist = read_SubaruTargetList(ref_file)
+
+# check matching of object_name
+# check matching of ra and dec within 1 min (< 100.00)  hhmmss.ss and +/-ddmmss.ss   
+
+for in_target in in_targetlist:
+	for ref_target in ref_targetlist:
+		duplicate_flag = False
+		ra_diff = abs(in_target["ra"]-ref_target["ra"])
+		dec_diff = abs(in_target["dec"]-ref_target["dec"])
+		if in_target["ojbect_name"] == ref_target["object_name"]:
+			duplicate_flag = True
+		elif (ra_diff < 100.0) and (dec_diff < 100.0):
+			duplicate_flag = True
+			
+		if duplicate_flag:
+			print()  # blank line
+			print(f"{in_target['object_name']} is duplicated with {ref_target['object_name']}.")
+			print(in_target)
+			print(ref_target)
+
